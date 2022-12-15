361af8ed0ce1fa408eef490e7f38a31088eed14e
woodysee <woodyseesk@gmail.com>
2022-12-16 00:01:19 +0800

    Use BASH_SOURCE[0] first

--- get_this_script_dir.sh
+++ get_this_script_dir.sh
@@ -4,11 +4,12 @@
 # The inclusion of output redirection (>/dev/null 2>&1) handles the rare(?)
 # case where cd might produce output that would interfere with the surrounding $( ... )
 # capture. (Such as cd being overridden to also ls a directory after switching to it.)
-# The -- after cd and before "$0" are in case the directory starts with a -.
+# The -- after cd and before "${BASH_SOURCE[0]:-$0}" are in case the directory starts with a -.
 # See https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
-get_this_script_dir() {
+# ${BASH_SOURCE[0]:-$0} in case `sh` is not aliased to `bash` and to another shell like `zsh`
+get_this_script_dir_when_no_symlinks() {
   echo $(
-    cd -- "$(dirname "$0")" >/dev/null 2>&1
+    cd -- "$(dirname ${BASH_SOURCE[0]}:-$0)" >/dev/null 2>&1
     pwd -P
   )
 }
